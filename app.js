e36eccd2ee4af05466f7246e480d2ab78902e2c7
Himanshusaini12 <himanshusaini962001@gmail.com>
2023-12-02 15:52:31 +0530

    d

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const client = new TelegramClient(stringSession, apiId, apiHash, {});
 app.use(express.json());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
